0d2b5c72bff4db35d69782f3e42c3ad5f0af2fc4
mooni9375 <moonmoon9375@gmail.com>
2024-11-30 16:25:33 +0900

    configuration changed

--- app-service/app-application/src/main/java/com/tradeinsurance/appapplication/rest/AppController.java
+++ app-service/app-application/src/main/java/com/tradeinsurance/appapplication/rest/AppController.java
@@ -1,4 +1,47 @@
 package com.tradeinsurance.appapplication.rest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/app-service")
 public class AppController {
+
+    private Environment env;
+
+    @Autowired
+    public AppController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("It's Working in App Service").append(System.lineSeparator());
+        stringBuilder.append("port(local.server.port) = " + env.getProperty("local.server.port")).append(System.lineSeparator());
+        stringBuilder.append("port(server.port) = " + env.getProperty("server.port")).append(System.lineSeparator());
+        stringBuilder.append("token secret = " + env.getProperty("token.secret")).append(System.lineSeparator());
+        stringBuilder.append("token expiration time = " + env.getProperty("token.expirationTime"));
+
+        log.info(stringBuilder.toString());
+
+        return stringBuilder.toString();
+
+
+//        return String.format("It's Working in App Service\n"
+//                        + "port(local.server.port) = " + env.getProperty("local.server.port" + "\n")
+//                        + "port(server.port) = " + env.getProperty("server.port" + "\n")
+//                        + "token secret = " + env.getProperty("token.secret" + "\n")
+//                        + "token expiration time = " + env.getProperty("token.expirationTime")
+//        );
+    }
+
+
 }
--- app-service/app-container/src/main/java/com/tradeinsurance/appcontainer/AppContainerApplication.java
+++ app-service/app-container/src/main/java/com/tradeinsurance/appcontainer/AppContainerApplication.java
@@ -3,8 +3,10 @@ package com.tradeinsurance.appcontainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.tradeinsurance.appapplication", "com.tradeinsurance.appcontainer"})
 @EnableDiscoveryClient
 public class AppContainerApplication {
 
