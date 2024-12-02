3ae5d846afc0b7e40ea721804ebdd2b3ecfb7290
mooni9375 <moonmoon9375@gmail.com>
2024-12-02 22:45:13 +0900

    Cloud Bus Config commit.

--- review-service/review-container/src/main/java/com/tradeinsurance/reviewcontainer/ReviewContainerApplication.java
+++ review-service/review-container/src/main/java/com/tradeinsurance/reviewcontainer/ReviewContainerApplication.java
@@ -2,8 +2,12 @@ package com.tradeinsurance.reviewcontainer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.tradeinsurance.reviewapplication", "com.tradeinsurance.reviewcontainer"})
+@EnableDiscoveryClient
 public class ReviewContainerApplication {
 
     public static void main(String[] args) {
